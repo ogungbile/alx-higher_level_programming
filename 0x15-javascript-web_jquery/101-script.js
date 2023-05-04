a9bf56fe8fb03f980c509ae0daf58dd2d6d5cf28
ogungbile <52929020+ogungbile@users.noreply.github.com>
2023-05-04 01:02:03 +0100

    Update 101-script.js

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,3 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('HEADER').style.color = '#FF0000';
+$(document).ready(function () {
+	$("#add_item").click(function () {
+		$("<li>").text("Item").appendTo("ul.my_list");
+	});
+	$("#remove_item").click(function () {
+		$("ul.my_list li:last-child").remove();
+	});
+	$("#clear_list").click(function () {
+		$("ul.my_list").empty();
+	});
 });
